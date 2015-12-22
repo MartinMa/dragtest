8fb058a153e328bcc8400e2b7cc12659996b8ee7
Martin Mädler <martin.maedler@gmail.com>
2015-12-22 09:49:02 +0100

    Fixed linter alerts

--- www/js/app.js
+++ www/js/app.js
@@ -1,32 +1,37 @@
+/* App Main File */
+
+/*global angular: true, console: true */
+
 angular.module('ionicApp', ['ionic'])
-.controller('MyCtrl', function ($scope, $ionicGesture) {
-	var onDrag, onRelease, dragElement, dragGesture, release, x, y;
-  
-  x = 0;
-  y = 0;
-  
-  dragElement = angular.element(document.getElementById("dragElement"));
-  
-  onDrag = function (event) {
-  	var deltaX, deltaY;
-    
-    deltaX = event.gesture.deltaX;
-    deltaY = event.gesture.deltaY;
-    
-    dragElement.css('margin-left', String(x + deltaX) + 'px');
-    dragElement.css('margin-top', String(y + deltaY) + 'px');
-  }
-  
-  onRelease = function (event) {
-    x = x + event.gesture.deltaX;
-    y = y + event.gesture.deltaY;
-  };
-  
-	dragGesture = $ionicGesture.on('drag', onDrag, dragElement);
-  release = $ionicGesture.on('release', onRelease, dragElement);
-  
-  $scope.$on('$destroy', function () {
-    $ionicGesture.off(dragGesture, 'drag', onDrag);
-    $ionicGesture.off(release, 'release', onRelease);
+  .controller('MyCtrl', function ($scope, $ionicGesture) {
+    'use strict';
+    var onDrag, onRelease, dragElement, dragGesture, release, x, y;
+
+    x = 0;
+    y = 0;
+
+    dragElement = angular.element(document.getElementById("dragElement"));
+
+    onDrag = function (event) {
+      var deltaX, deltaY;
+
+      deltaX = event.gesture.deltaX;
+      deltaY = event.gesture.deltaY;
+
+      dragElement.css('margin-left', String(x + deltaX) + 'px');
+      dragElement.css('margin-top', String(y + deltaY) + 'px');
+    };
+
+    onRelease = function (event) {
+      x = x + event.gesture.deltaX;
+      y = y + event.gesture.deltaY;
+    };
+
+    dragGesture = $ionicGesture.on('drag', onDrag, dragElement);
+    release = $ionicGesture.on('release', onRelease, dragElement);
+
+    $scope.$on('$destroy', function () {
+      $ionicGesture.off(dragGesture, 'drag', onDrag);
+      $ionicGesture.off(release, 'release', onRelease);
+    });
   });
-});
