8d6f6a24d470859c24e00ef3bb133bb021152738
Martin Mädler <martin.maedler@gmail.com>
2015-12-22 09:44:47 +0100

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+angular.module('ionicApp', ['ionic'])
+.controller('MyCtrl', function ($scope, $ionicGesture) {
+	var onDrag, onRelease, dragElement, dragGesture, release, x, y;
+  
+  x = 0;
+  y = 0;
+  
+  dragElement = angular.element(document.getElementById("dragElement"));
+  
+  onDrag = function (event) {
+  	var deltaX, deltaY;
+    
+    deltaX = event.gesture.deltaX;
+    deltaY = event.gesture.deltaY;
+    
+    dragElement.css('margin-left', String(x + deltaX) + 'px');
+    dragElement.css('margin-top', String(y + deltaY) + 'px');
+  }
+  
+  onRelease = function (event) {
+    x = x + event.gesture.deltaX;
+    y = y + event.gesture.deltaY;
+  };
+  
+	dragGesture = $ionicGesture.on('drag', onDrag, dragElement);
+  release = $ionicGesture.on('release', onRelease, dragElement);
+  
+  $scope.$on('$destroy', function () {
+    $ionicGesture.off(dragGesture, 'drag', onDrag);
+    $ionicGesture.off(release, 'release', onRelease);
+  });
+});
